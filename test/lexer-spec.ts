60b8bf7007ce05266dc5034dddba03c986a5f07d
Ryan Kennedy <r24y@users.noreply.github.com>
2017-07-24 18:39:17 -0400

    Make lexer test more robust

--- test/lexer-spec.ts
+++ test/lexer-spec.ts
@@ -1,6 +1,10 @@
 import test from "ava";
 import HclLexer from '../src/lexer';
 
+function stringChars(str: string, type = 'stringChar') {
+  return str.split('').map((c: string) => [type, c]);
+}
+
 test('Should find tokens correctly', t => {
   const SOURCE_TEXT = `
 foo = {
@@ -23,5 +27,49 @@ more_content = true
     output.push(token);
   }
 
-  t.snapshot(output);
-})
+  t.deepEqual(
+    output.filter(token => token.type !== 'ws')
+      .map(token => [token.type, token.value]),
+    [
+      ['identifier', 'foo'],
+      ['equal', '='],
+      ['openBrace', '{'],
+      ['identifier', 'bar'],
+      ['equal', '='],
+      ['beginString', '"'],
+      ...stringChars('baz '),
+      ['escapedDollar', '$$'],
+      ...stringChars('{quux} '),
+      ['beginInterpolation', '${'],
+      ['identifier', 'var'],
+      ['dot', '.'],
+      ['identifier', 'mumble'],
+      ['endInterpolation', '}'],
+      ['endString', '"'],
+      ['identifier', 'quux'],
+      ['equal', '='],
+      ['baseTenNumber', '42'],
+      ['closeBrace', '}'],
+      ['beginLineComment', '//'],
+      ...stringChars(' Example of a heredoc', 'commentText'),
+      ['endComment', '\n'],
+      ['identifier', 'mumble'],
+      ['equal', '='],
+      ['beginHeredoc', '<<EOF\n'],
+      ...stringChars('foo bar '),
+      ['beginInterpolation', '${'],
+      ['identifier', 'baz'],
+      ['openParen', '('],
+      ['baseTenNumber', '2'],
+      ['plus', '+'],
+      ['baseTenNumber', '2'],
+      ['closeParen', ')'],
+      ['endInterpolation', '}'],
+      ['newline', '\n'],
+      ['endHeredoc', 'EOF\n'],
+      ['identifier', 'more_content'],
+      ['equal', '='],
+      ['boolean', 'true'],
+    ]
+  );
+});
