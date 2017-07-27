6b3e13fd90c02eb358621607109a554990782886
Ryan Kennedy <r24y@users.noreply.github.com>
2017-07-27 16:24:52 -0400

    Cover last few lexer branches

--- test/lexer.spec.ts
+++ test/lexer.spec.ts
@@ -74,3 +74,56 @@ more_content = true
     ]
   );
 });
+
+
+test('Should save state correctly', t => {
+  // Note that the source text ends in the middle of a heredoc
+  const SOURCE_TEXT = `
+foo = {
+  bar = "baz \$\${quux} \${var.mumble}"
+  quux = 42
+}
+
+// Example of a heredoc
+mumble = <<EOF
+foo bar \${baz(2 + 2)}`;
+
+  const lex = new HclLexer(SOURCE_TEXT);
+
+  const output = [];
+  let token;
+
+  while ((token = lex.next())) {
+    output.push(token);
+  }
+
+  t.snapshot(output);
+
+  const state = lex.save();
+
+  const newLex = new HclLexer();
+  // Resume lexing from where we left off: middle of heredoc
+  newLex.reset('\nEOF\n\nmore_content=true', state);
+  while ((token = newLex.next())) {
+    output.push(token);
+  }
+
+  t.snapshot(output, 'after break');
+});
+
+
+test('Should format errors correctly', t => {
+  const SOURCE_TEXT = `
+foo = {
+  bar = "baz \$\${quux} \${var.mumble}"
+  quux = 42
+}
+`;
+
+  const lex = new HclLexer(SOURCE_TEXT);
+
+  const token = lex.next()
+
+  t.snapshot(lex.formatError(token));
+
+});
